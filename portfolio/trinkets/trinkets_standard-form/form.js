9bcb2ba6fb1fa618e5b6d79b7663f038a0dc530b
Gong Zehua <55455296+goot4@users.noreply.github.com>
2024-03-28 18:47:48 +0800

    clean up
    remove blog part

--- portfolio/trinkets/trinkets_standard-form/form.js
+++
@@ -1,8 +0,0 @@
-const appetite = document.querySelector("#appetite");
-const output = document.querySelector(".appetite-output");
-
-output.textContent = `${appetite.value}g`;
-
-appetite.addEventListener("input", () => {
-    output.textContent = `${appetite.value}g`;
-});
