d0d26aced68c752ce878e35109cb8877b0f968ca
Gong Zehua <55455296+goot4@users.noreply.github.com>
2024-04-19 14:28:48 +0800

    Home page finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,21 @@
-
+import Welcome from "@/content/welcome.mdx";
+// Home page
 export default function Page() {
-  return <h1 className="bg-amber-600">Hello, Next.js!</h1>
+  return (
+    <div className="flex h-screen flex-row items-start bg-green-300">
+      <div className="w-full rounded-md bg-zinc-800/50 px-6 py-16 text-white md:ml-16 md:mt-4 md:w-1/3">
+        <Welcome />
+      </div>
+      <div className="hidden h-full flex-grow flex-col space-x-reverse space-x-8 p-16 text-3xl [writing-mode:vertical-rl] md:flex">
+        <p>Looking for a job</p>
+        <p>求職中</p>
+        <p>求职中</p>
+        <p>仕事を探している</p>
+        <p>일자리를 찾고</p>
+        <p>Ищу работу</p>
+        <p>🏬🚶💻🤝👌</p>
+        <p>...</p>
+      </div>
+    </div>
+  );
 }
