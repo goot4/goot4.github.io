d4c25744eac5245d1ea631731a2261c0a8eaf020
kyo2 <kyonoka@outlook.com>
2024-10-01 03:03:58 +0800

    update infos

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,20 @@ import Welcome from "@/content/welcome.mdx";
 // Home page
 export default function Page() {
   return (
-    <div className="flex h-screen pt-16 flex-row items-start bg-green-300">
+    <div className="flex h-screen flex-row items-start bg-green-300 pt-16">
       <div className="w-full rounded-md bg-zinc-800/50 px-6 py-16 text-white md:ml-16 md:mt-4 md:w-1/3">
         <Welcome />
       </div>
-      <div className="hidden h-full flex-grow flex-col space-x-reverse space-x-8 p-16 text-3xl [writing-mode:vertical-rl] md:flex">
-        <p>Looking for a job</p>
-        <p>求職中</p>
-        <p>求职中</p>
-        <p>仕事を探している</p>
-        <p>일자리를 찾고</p>
-        <p>Ищу работу</p>
-        <p>🏬🚶💻🤝👌</p>
+      <div
+        className="hidden h-full flex-grow flex-col space-x-8 space-x-reverse p-16 text-3xl [writing-mode:vertical-rl] md:flex">
+        <p>核心能力 - 前端交互开发</p>
+        <p>创新能力 - 跨领域的知识体系</p>
+        <p>成长中 - 两年</p>
+        <p>广泛的兴趣</p>
+        <p>持续的学习</p>
+        <p>长期的目标</p>
+        <p>及时的反馈</p>
+        <p>拥抱不确定性</p>
         <p>...</p>
       </div>
     </div>
